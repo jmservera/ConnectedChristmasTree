0e6e4ff65cb751e6d3415a12853aa1fc9ada67ce
Juan Manuel Servera <jmservera@outlook.com>
2015-12-16 15:08:01 +0100

    Full communication between face and tree

---
+++ EmotionDetector/EmotionDetector/EmotionResult.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmotionDetector
+{
+    public class EmotionResult
+    {
+        public string Id { get; set; }
+
+        public Guid SessionId { get; set; }
+        public DateTime Date { get; set; }
+        public string Emotion { get; set; }
+        public double Score { get; set; }
+
+    }
+}
--- EmotionDetector/EmotionDetector/MainPage.xaml.cs
+++ EmotionDetector/EmotionDetector/MainPage.xaml.cs
@@ -1,10 +1,12 @@
-﻿using Microsoft.ProjectOxford.Emotion;
+﻿using Microsoft.Azure.Devices.Client;
+using Microsoft.ProjectOxford.Emotion;
 using Microsoft.ProjectOxford.Vision;
 using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Runtime.InteropServices.WindowsRuntime;
+using System.Text;
 using System.Threading.Tasks;
 using Windows.Devices.Enumeration;
 using Windows.Devices.Gpio;
@@ -36,10 +38,18 @@ namespace EmotionDetector
         DispatcherTimer dispatcherTimer = new DispatcherTimer() { Interval = TimeSpan.FromSeconds(1) };
         EmotionServiceClient emotionClient = new EmotionServiceClient(EmotionAPIKey);
         VisionServiceClient visionClient = new VisionServiceClient(VisionAPIKey);
+        DeviceClient deviceClient;
+
 
         UltrasonicDistanceSensor distanceSensor = new UltrasonicDistanceSensor(23, 24);
+        System.Threading.ManualResetEventSlim signal=new System.Threading.ManualResetEventSlim(false);
 
-        Windows.Devices.Gpio.GpioPin buttonPin;
+
+        GpioPin buttonPin;
+
+        const string iotHubUri = "CCTPragueIOtHub.azure-devices.net";
+        const string deviceName = "EmotionDetector";
+        const string deviceKey = "XIakbI1GwhMBtGbdpCcTocl6ecK1H95eIKAXYtHQ4e8=";
 
         public MainPage()
         {
@@ -64,16 +74,40 @@ namespace EmotionDetector
                 while (true)
                 {
                     await Task.Delay(1000);
+                    log("waiting for person");
                     await waitForPerson();
+                    log("person detected, getting emotion");
+                    var guid = Guid.NewGuid();
                     //take a picture
-                    //dispatcherTimer.Tick += GetEmotions;
-                    //dispatcherTimer.Start();
-
+                    var emotion=await GetEmotion();
+                    log($"emotion detected: {emotion.Emotion} {emotion.Score}");
+                    emotion.SessionId = guid;
                     //send a message to the tree: lights on
 
-                    // take another picture
+                    var emotionJson= Newtonsoft.Json.JsonConvert.SerializeObject(emotion);
+                    Message m = new Message(Encoding.UTF8.GetBytes(emotionJson));
+                    m.To = "Tree";
+                    log("Sending to tree");
+                    await deviceClient.SendEventAsync(m);
+                    log("Message sent");
+                    //wait for the tree lights
+                    await Task.Run(() =>
+                    {
+                        signal.Wait();
+                    });
+                    log("Signal received, taking new emotion");
+                    signal.Reset();
 
-                    //send a message to the tree: change lights depending on mood
+                    emotion = await GetEmotion();
+                    emotion.SessionId = guid;
+                    log($"new emotion detected: {emotion.Emotion} {emotion.Score}");
+
+                    emotionJson = Newtonsoft.Json.JsonConvert.SerializeObject(emotion);
+                    m = new Message(Encoding.UTF8.GetBytes(emotionJson));
+                    m.To = "Tree";
+
+                    await deviceClient.SendEventAsync(m);
+                    log("New emotion sent to tree");
 
                 }
             }
@@ -113,57 +147,73 @@ namespace EmotionDetector
 
         private async Task<bool> Init()
         {
-            try {
-                //sensor initialization
-                await distanceSensor.InitAsync();
-
-                //media capture initialization
-                log("finding devices");
-                mediaCapture = new MediaCapture();
-                var cameras = await DeviceInformation.FindAllAsync(DeviceClass.VideoCapture);
-                var camera = cameras.First();
-                log("initializing");
-                var settings = new MediaCaptureInitializationSettings() { VideoDeviceId = camera.Id };
-                await mediaCapture.InitializeAsync(settings);
-                log("initialized");
-                var streamprops = mediaCapture.VideoDeviceController.GetMediaStreamProperties(MediaStreamType.Photo)
-                    as VideoEncodingProperties;
-                width = streamprops.Width;
-                height = streamprops.Height;
-                ViewFinder.Source = mediaCapture;
-                await mediaCapture.StartPreviewAsync();
-                log("preview started");
+            try
+            {
+                var init1= initSensor();
+                var init2= initCamera();
+                await Task.WhenAll(init1, init2);
+                initHub();
                 return true;
             }
-            catch(Exception ex)
+            catch (Exception ex)
             {
                 log(ex.Message);
             }
             return false;
         }
 
-        async void GetEmotions(object sender, object e)
+        private void initHub()
         {
-            dispatcherTimer.Stop();
-            double ratio = 1;
-            double leftMargin = 0;
-            double topMargin = 0;
-            if (width > 0)
-            {
-                var hratio = ViewFinder.ActualHeight / height;
-                var wratio = ViewFinder.ActualWidth / width;
-                if (hratio < wratio)
-                {
-                    ratio = hratio;
-                    leftMargin = (ViewFinder.ActualWidth - (width * ratio)) / 2;
-                }
-                else
-                {
-                    ratio = wratio;
-                    topMargin = (ViewFinder.ActualHeight - (height * ratio)) / 2;
+            var key = AuthenticationMethodFactory.CreateAuthenticationWithRegistrySymmetricKey(deviceName, deviceKey);
+            deviceClient = DeviceClient.Create(iotHubUri, key, TransportType.Http1);
 
+            startReceiving();
+        }
+
+        private async void startReceiving()
+        {
+            while (true)
+            {
+                var message = await deviceClient.ReceiveAsync();
+                if(message!= null)
+                {
+                    var jsonMessage = Encoding.UTF8.GetString(message.GetBytes());
+                    if(jsonMessage!= null)
+                    {
+                        signal.Set();
+                    }
                 }
             }
+        }
+
+        private async Task initCamera()
+        {
+            //media capture initialization
+            log("finding devices");
+            mediaCapture = new MediaCapture();
+            var cameras = await DeviceInformation.FindAllAsync(DeviceClass.VideoCapture);
+            var camera = cameras.First();
+            log("initializing");
+            var settings = new MediaCaptureInitializationSettings() { VideoDeviceId = camera.Id };
+            await mediaCapture.InitializeAsync(settings);
+            log("initialized");
+            var streamprops = mediaCapture.VideoDeviceController.GetMediaStreamProperties(MediaStreamType.Photo)
+                as VideoEncodingProperties;
+            width = streamprops.Width;
+            height = streamprops.Height;
+            ViewFinder.Source = mediaCapture;
+            await mediaCapture.StartPreviewAsync();
+            log("preview started");
+        }
+
+        private async Task initSensor()
+        {
+            //sensor initialization
+            await distanceSensor.InitAsync();
+        }
+
+        async Task<EmotionResult> GetEmotion()
+        {
             try
             {
                 log("Capturing photo");
@@ -171,89 +221,31 @@ namespace EmotionDetector
                 {
                     await mediaCapture.CapturePhotoToStreamAsync(ImageEncodingProperties.CreateJpeg(), mediaStream.AsRandomAccessStream());
                     var streamCopy = new MemoryStream();
-                    try
+                    mediaStream.Position = 0L;
+                    log("Getting emotion");
+                    var emotions = await emotionClient.RecognizeAsync(mediaStream);
+
+                    if (emotions != null && emotions.Length > 0)
                     {
-                        mediaStream.Position = 0L;
-                        mediaStream.CopyTo(streamCopy);
-                        mediaStream.Position = 0L;
-                        streamCopy.Position = 0L;
-                        log("Getting emotion");
+                        log("Emotion recognized");
+                        var nearestOne = emotions.OrderByDescending(emotion => emotion.FaceRectangle.Height * emotion.FaceRectangle.Width).First();
 
-                        var emotionQuery = emotionClient.RecognizeAsync(mediaStream);
-                        var visionQuery = visionClient.AnalyzeImageAsync(streamCopy);
 
-                        await Task.WhenAll(emotionQuery, visionQuery);
+                        var list = new[] { new { Emotion="Anger", Score= nearestOne.Scores.Anger },
+                                new { Emotion="Contempt", Score= nearestOne.Scores.Contempt},
+                                new { Emotion="Disgust", Score= nearestOne.Scores.Disgust },
+                                new { Emotion="Fear", Score= nearestOne.Scores.Fear },
+                                new { Emotion="Happiness", Score= nearestOne.Scores.Happiness },
+                                new { Emotion="Neutral", Score= nearestOne.Scores.Neutral },
+                                new { Emotion="Sadness", Score= nearestOne.Scores.Sadness },
+                                new { Emotion="Surprise", Score= nearestOne.Scores.Surprise }};
 
-                        var emotions = emotionQuery.Result;
-                        var vision = visionQuery.Result;
-
-                        if (emotions != null && emotions.Length > 0)
-                        {
-                            log("Emotion recognized");
-                            RectangleCanvas.Children.Clear();
-                            foreach (var emotion in emotions)
-                            {
-                                var r = new Windows.UI.Xaml.Shapes.Rectangle();
-                                RectangleCanvas.Children.Add(r);
-                                r.Stroke = new SolidColorBrush(Windows.UI.Colors.Yellow);
-                                r.StrokeThickness = 5;
-                                r.Width = emotion.FaceRectangle.Width * ratio;
-                                r.Height = emotion.FaceRectangle.Height * ratio;
-                                Canvas.SetLeft(r, (emotion.FaceRectangle.Left * ratio) + leftMargin);
-                                Canvas.SetTop(r, (emotion.FaceRectangle.Top * ratio) + topMargin);
-                                var t = new TextBlock();
-                                RectangleCanvas.Children.Add(t);
-                                t.Width = r.Width;
-                                t.FontSize = 20;
-                                t.Foreground = new SolidColorBrush(Windows.UI.Colors.Yellow);
-                                Canvas.SetLeft(t, (emotion.FaceRectangle.Left * ratio) + leftMargin);
-                                Canvas.SetTop(t, (emotion.FaceRectangle.Top * ratio) + topMargin + r.Height);
-
-                                var list = new[] { new { Emotion="Anger", Score= emotion.Scores.Anger },
-                                new { Emotion="Contempt", Score= emotion.Scores.Contempt},
-                                new { Emotion="Disgust", Score= emotion.Scores.Disgust },
-                                new { Emotion="Fear", Score= emotion.Scores.Fear },
-                                new { Emotion="Happiness", Score= emotion.Scores.Happiness },
-                                new { Emotion="Neutral", Score= emotion.Scores.Neutral },
-                                new { Emotion="Sadness", Score= emotion.Scores.Sadness },
-                                new { Emotion="Surprise", Score= emotion.Scores.Surprise }
-                            };
-                                var max = list.ToList().OrderByDescending(a => a.Score);
-                                if (max.First().Score < 0.8)
-                                {
-                                    t.Text = string.Format("{0}:{1:P}\n{2}:{3:P}", max.First().Emotion, max.First().Score,
-                                        max.Skip(1).First().Emotion, max.Skip(1).First().Score);
-                                }
-                                else
-                                {
-                                    t.Text = string.Format("{0}:{1:P}", max.First().Emotion, max.First().Score);
-                                }
-
-                            }
-                        }
-                        else
-                        {
-                            log("emotion not recognized");
-                        }
-
-                        if (vision != null)
-                        {
-                            foreach (var face in vision.Faces)
-                            {
-                                var t = new TextBlock();
-                                RectangleCanvas.Children.Add(t);
-                                t.Width = face.FaceRectangle.Width * ratio;
-                                t.FontSize = 20;
-                                t.Foreground = new SolidColorBrush(Windows.UI.Colors.Yellow);
-                                Canvas.SetLeft(t, (face.FaceRectangle.Left * ratio) + leftMargin);
-                                Canvas.SetTop(t, (face.FaceRectangle.Top * ratio) + topMargin - 30);
-                                t.Text = $"Age: {face.Age} Gender: {face.Gender}";
-                            }
-                        }
+                        var max = list.ToList().OrderByDescending(a => a.Score).First();
+                        return new EmotionResult {Id="12", Date = DateTime.Now, Emotion = max.Emotion, Score = max.Score };
                     }
-                    finally
+                    else
                     {
-                        streamCopy.Dispose();
+                        log("emotion not recognized");
                     }
                 }
             }
@@ -261,10 +253,7 @@ namespace EmotionDetector
             {
                 log(ex.Message);
             }
-            finally
-            {
-                dispatcherTimer.Start();
-            }
+            return null;
         }
 
         private async void log(string message)
