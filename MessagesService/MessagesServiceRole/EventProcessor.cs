5fbb0cdf15fe3541dc0936e4be73480eaebde362
Juan Manuel Servera <jmservera@outlook.com>
2016-02-21 01:49:26 +0100

    add eventprocessorhost

---
+++ MessagesService/MessagesServiceRole/EventProcessor.cs
@@ -0,0 +1,96 @@
+﻿using Microsoft.Azure;
+using Microsoft.Azure.Devices;
+using Microsoft.ServiceBus.Messaging;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MessagesServiceRole
+{
+    public class EventProcessor : IEventProcessor
+    {
+        public PartitionContext Context { get; private set; }
+        public event EventHandler ProcessorClosed;
+        public bool IsInitialized { get; private set; }
+        public bool IsClosed { get; private set; }
+
+        public bool IsReceivedMessageAfterClose { get; set; }
+        ServiceClient client;
+        public EventProcessor()
+        {
+            var iotHubConnectionString = CloudConfigurationManager.GetSetting("IoTHubConnectionString");
+            client = ServiceClient.CreateFromConnectionString(iotHubConnectionString);
+        }
+
+        public Task CloseAsync(PartitionContext context, CloseReason reason)
+        {
+            Trace.TraceInformation("Processor Shuting Down.  Partition '{0}', Reason: '{1}'.", this.Context.Lease.PartitionId, reason.ToString());
+
+            this.IsClosed = true;
+            this.OnProcessorClosed();
+
+            return context.CheckpointAsync();
+        }
+
+        protected async virtual void OnProcessorClosed()
+        {
+            await client.CloseAsync();
+            if (this.ProcessorClosed != null)
+            {
+                this.ProcessorClosed(this, EventArgs.Empty);
+            }
+        }
+
+        public async Task OpenAsync(PartitionContext context)
+        {
+            Trace.TraceInformation("SimpleEventProcessor initialize.  Partition: '{0}', Offset: '{1}'", context.Lease.PartitionId, context.Lease.Offset);
+
+            await client.OpenAsync();
+
+            this.Context = context;
+            this.IsInitialized = true;
+
+            Trace.TraceInformation("SimpleEventProcessor initialized!!!  Partition: '{0}', Offset: '{1}'", context.Lease.PartitionId, context.Lease.Offset);
+        }
+
+        public async Task ProcessEventsAsync(PartitionContext context, IEnumerable<EventData> messages)
+        {
+            try
+            {
+                Trace.TraceInformation("Processing event hub data for {0} messages...", messages.Count());
+
+                var toMessages = messages.Where((m) =>
+                {
+                    return m.SystemProperties.ContainsKey("to") && m.SystemProperties["to"] != null;
+                });
+                foreach (var m in toMessages)
+                {
+                    Message message = new Message(m.GetBytes());
+                    message.Ack = DeliveryAcknowledgement.Full;
+                    message.MessageId = Guid.NewGuid().ToString();
+                    await client.SendAsync(m.SystemProperties["to"].ToString(), message);
+                    //log($"Message sent to {m.SystemProperties["to"].ToString()}");
+                }
+
+                var treeMessages = messages.Where((m) =>
+                {
+                    string s = Encoding.UTF8.GetString(m.GetBytes());
+                    return s != null && s.Contains("Heartrate");
+                });
+
+                if (this.IsClosed)
+                {
+                    this.IsReceivedMessageAfterClose = true;
+                }
+                await context.CheckpointAsync();
+            }
+            catch (Exception exp)
+            {
+                Trace.TraceError("Error in processing: {0}", exp.Message);
+            }
+        }
+    }
+}
--- MessagesService/MessagesServiceRole/WorkerRole.cs
+++ MessagesService/MessagesServiceRole/WorkerRole.cs
@@ -13,6 +13,7 @@ using Microsoft.Azure.Devices;
 using Microsoft.ServiceBus.Messaging;
 using Microsoft.Azure.Devices.Common;
 using System.Text;
+using Microsoft.Azure;
 
 namespace MessagesServiceRole
 {
@@ -21,7 +22,7 @@ namespace MessagesServiceRole
         private readonly CancellationTokenSource cancellationTokenSource = new CancellationTokenSource();
         private readonly ManualResetEvent runCompleteEvent = new ManualResetEvent(false);
         string iotHubConnectionString;
-        string eventHubConnectionString;
+        string storageConnectionString;
 
         public override void Run()
         {
@@ -56,110 +57,36 @@ namespace MessagesServiceRole
         private void readSettings()
         {
             iotHubConnectionString = CloudConfigurationManager.GetSetting("IoTHubConnectionString");
-            eventHubConnectionString = CloudConfigurationManager.GetSetting("EventHubConnectionString");
+            storageConnectionString = CloudConfigurationManager.GetSetting("storageConnectionString");
         }
 
-        public override void OnStop()
+        public override async void OnStop()
         {
             Trace.TraceInformation("MessagesServiceRole is stopping");
 
-            this.cancellationTokenSource.Cancel();
-            this.runCompleteEvent.WaitOne();
-
+            await host.UnregisterEventProcessorAsync();
             base.OnStop();
 
             Trace.TraceInformation("MessagesServiceRole has stopped");
         }
-
+        EventProcessorHost host;
         private async Task RunAsync(CancellationToken cancellationToken)
         {
-            ServiceClient client = ServiceClient.CreateFromConnectionString(iotHubConnectionString);
-            await client.OpenAsync();
-            List<Task> streamTasks = new List<Task>();
+            var builder = IotHubConnectionStringBuilder.Create(iotHubConnectionString);
+            var hubName = builder.HostName.Split('.')[0];
+            string eventHubPath = "messages/events";
+            host = new EventProcessorHost("Worker RoleId: " + RoleEnvironment.CurrentRoleInstance.Id,
+                eventHubPath, EventHubConsumerGroup.DefaultGroupName, iotHubConnectionString,
+                storageConnectionString,"messagesevents");
 
-            streamTasks.AddRange(await 
-                createEventHubClients(client,eventHubConnectionString, "ioteventhubcct", null,cancellationToken));
-            streamTasks.AddRange(await
-                createEventHubClients(client, iotHubConnectionString, "messages/events", "message",cancellationToken));
-           
-            await Task.WhenAll(streamTasks);
-        }
-
-        private async Task<IEnumerable<Task>> createEventHubClients(ServiceClient client,
-            string eventHubConnectionString, string eventHubPath, string consumerGroup, CancellationToken token)
-        {
-            List<Task> tasks = new List<Task>();
-            var streamClient = EventHubClient.CreateFromConnectionString(eventHubConnectionString, eventHubPath);
-            var streamClientInfo = streamClient.GetRuntimeInformation();
-            var partitions = streamClientInfo.PartitionIds;
-            foreach (var partition in partitions)
-            {
-                log($"Create {eventHubPath} event reciever for partition: {partition}");
-
-                EventHubReceiver partReceiver;
-                if (string.IsNullOrEmpty(consumerGroup))
-                {
-                    partReceiver = await streamClient.GetDefaultConsumerGroup().CreateReceiverAsync(partition, DateTime.Now);
-                }
-                else
-                {
-                    partReceiver = await streamClient.GetConsumerGroup(consumerGroup).CreateReceiverAsync(partition, DateTime.Now);
-
-                }
-                Task task = createReceiverTask(client, partReceiver, token);
-                tasks.Add(task);
-            }
-            return tasks;
-        }
-
-        private static Task createReceiverTask(ServiceClient client, EventHubReceiver receiver, CancellationToken cancellationToken)
-        {
-            return Task.Run(async () =>
-            {
-                while (!cancellationToken.IsCancellationRequested)
-                {
-                    try {
-                        var messages = await receiver.ReceiveAsync(20);
-                        await resolveMessages(client, messages);
-                        await Task.Delay(1000);
-                    }
-                    catch(Exception ex)
-                    {
-                        log(ex.Message);
-                    }
-                }
-            }, cancellationToken);
-        }
-
-        private static async Task resolveMessages(ServiceClient client, IEnumerable<EventData> messages)
-        {
-            var toMessages = messages.Where((m) =>
-            {
-                return m.SystemProperties.ContainsKey("to") && m.SystemProperties["to"] != null;
+            await host.RegisterEventProcessorAsync<EventProcessor>(new EventProcessorOptions {
+                InitialOffsetProvider = (partition) => DateTime.UtcNow
             });
-            foreach (var m in toMessages)
+            while (!cancellationToken.IsCancellationRequested)
             {
-                Message message = new Message(m.GetBytes());
-                message.Ack = DeliveryAcknowledgement.Full;
-                message.MessageId = Guid.NewGuid().ToString();
-                await client.SendAsync(m.SystemProperties["to"].ToString(), message);
-                log($"Message sent to {m.SystemProperties["to"].ToString()}");
+                //Trace.TraceInformation("Working");
+                await Task.Delay(1000);
             }
-
-            var treeMessages = messages.Where((m) =>
-            {
-                string s = Encoding.UTF8.GetString(m.GetBytes());
-                return s != null && s.Contains("Heartrate");
-            });
-            foreach (var t in treeMessages)
-            {
-                Message message = new Message(t.GetBytes());
-                message.Ack = DeliveryAcknowledgement.Full;
-                message.MessageId = Guid.NewGuid().ToString();
-                await client.SendAsync("Tree", message);
-                log($"Tree Message sent {message}");
-            }
-
         }
 
         private static void log(string message)
